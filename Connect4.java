d3f33280c516fe12ca54911de34ea8dccbcb5199
messejak22 <70961425+messejak22@users.noreply.github.com>
2020-11-10 20:29:45 -0500

    Add files via upload
    This commit has a bug where when a piece is placed it off sets the board.

--- Connect4.java
+++ Connect4.java
@@ -1,36 +1,60 @@
 public class Connect4 {
 
+    // global variables
+    public static int playerOneChoice;
+    public static int columnsLen = 7;
+    public static int rowsLen = 6;
+    public static char[][] board = new char[rowsLen][columnsLen];
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        // prints board
+        printBoard();
 
-		board();
-		PlayerOne p1 = new PlayerOne();
-		System.out.println();
-		p1.gamePlay();
+        // retrieves input for player one
+        PlayerOne p1 = new PlayerOne();
+        System.out.println();
+        playerOneChoice = p1.gamePlay();
+        placePiece();
+    }
 
+    public static void printBoard() {
+        System.out.println("     A   B   C   D   E   F   G   ");
+        for (int i = 0; i < rowsLen; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < columnsLen; j++) {
+                System.out.print("  | " + board[i][j]);
 
-	}
+            }
+            System.out.print("  |\n");
 
-
-	public static void board() {
-		
-		int columnsLen = 7;
-		int rowsLen = 6;	
-		char[][] board = new char[rowsLen][columnsLen];
-		System.out.println("     A   B   C   D   E   F   G   ");
-		for (int i = 0; i < rowsLen; i++) {
-			System.out.print(i + 1);	
-			for (int j = 0; j < columnsLen; j++) {
-				System.out.print("  | " + board[i][j]);
-				
-			}
-			System.out.print("  |\n");
-		
-		} 
+        }
 
 
+    }
 
-	}
+    public static void placePiece() {
+
+        // places piece according to choice
+        if (playerOneChoice == 1) {
+            board[5][0] = 'R';
+        } else if (playerOneChoice == 2) {
+            board[5][1] = 'R';
+        } else if (playerOneChoice == 3) {
+            board[5][2] = 'R';
+        } else if (playerOneChoice == 4) {
+            board[5][3] = 'R';
+        } else if (playerOneChoice == 5) {
+            board[5][4] = 'R';
+        } else if (playerOneChoice == 6) {
+            board[5][5] = 'R';
+        } else if (playerOneChoice == 7) {
+            board[5][6] = 'R';
+        }
+        printBoard();
+
+    }
+
+
 
 
 }
